cc53fc79c297f4728f40cd46e16270b7d64381ba
dv-jamie <sjm2506@gmail.com>
2023-10-11 09:30:58 +0900

    style : 스크롤바 숨기기 취소

--- src/component/templates/Layout.js
+++ src/component/templates/Layout.js
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from "./Layout.module.css";
 
 function Layout({ language }) {
     return (
-        <div>
+        <div className={styles.container}>
             <Header language={language} />
             <Outlet />
             <Footer />
