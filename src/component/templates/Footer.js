d7f487434746d41151c438b2c55d844a76842e12
dv-jamie <sjm2506@gmail.com>
2023-10-12 21:26:45 +0900

    style : header, footer 여백 적용

--- src/component/templates/Footer.js
+++ src/component/templates/Footer.js
@@ -3,19 +3,21 @@ import styles from './Footer.module.css'
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <ul className={styles.list_wrap}>
-                <li>㈜에이비웍스</li>
-                <li>
-                    <span>주소&#32;&#32;|</span>
-                    <span>경기도 성남시 중원구 양현로 405번길 7 (여수동), 신야탑푸르지오시티2, 801호</span>
-                </li>
-                <li>
-                    <span>문의&#32;&#32;|</span>
-                    <span>info@aiviworks.com</span>
-                </li>
-            </ul>
+            <div className={styles.footer_inner}>
+                <ul className={styles.list_wrap}>
+                    <li>㈜에이비웍스</li>
+                    <li>
+                        <span>주소&#32;&#32;|</span>
+                        <span>경기도 성남시 중원구 양현로 405번길 7 (여수동), 신야탑푸르지오시티2, 801호</span>
+                    </li>
+                    <li>
+                        <span>문의&#32;&#32;|</span>
+                        <span>info@aiviworks.com</span>
+                    </li>
+                </ul>
 
-            <p className={styles.copyright}>Copyright 2023, AIVIWORKS all rights reserved.</p>
+                <p className={styles.copyright}>Copyright 2023, AIVIWORKS all rights reserved.</p>
+            </div>
         </footer>
     );
 }
