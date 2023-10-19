30e48a2ea4d1ff6024e98e62bc1059e774858236
dv-jamie <sjm2506@gmail.com>
2023-10-19 11:48:52 +0900

    style : footer 번역 작업 완료

--- src/component/templates/Footer.js
+++ src/component/templates/Footer.js
@@ -1,18 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import styles from './Footer.module.css'
 
 function Footer() {
+    const { t } = useTranslation("footer")
+
     return (
         <footer className={styles.footer}>
             <div className={styles.footer_inner}>
                 <ul className={styles.list_wrap}>
-                    <li>㈜에이비웍스</li>
+                    <li>{t("name")}</li>
                     <li>
-                        <span>주소&#32;&#32;|</span>
-                        <span>경기도 성남시 중원구 양현로 405번길 7 (여수동), 신야탑푸르지오시티2, 801호</span>
+                        <span>{t("list_1.label")}</span>
+                        <span>{t("list_1.desc")}</span>
                     </li>
                     <li>
-                        <span>문의&#32;&#32;|</span>
-                        <span>contact@aiviworks.com </span>
+                        <span>{t("list_2.label")}</span>
+                        <span>{t("list_2.desc")}</span>
                     </li>
                 </ul>
 
--- src/lang/i18n.js
+++ src/lang/i18n.js
@@ -6,21 +6,25 @@ import LanguaeDetector from "i18next-browser-languagedetector";
 import HomeKo from "./ko/home.json";
 import ProductKo from "./ko/product.json";
 import ContactKo from "./ko/contact.json";
+import FooterKo from "./ko/footer.json";
 
 import HomeEn from "./en/home.json";
 import ProductEn from "./en/product.json";
 import ContactEn from "./en/contact.json";
+import FooterEn from "./en/footer.json";
 
 const resources = {
   ko: {
     home: HomeKo,
     product: ProductKo,
     contact: ContactKo,
+    footer: FooterKo,
   },
   en: {
     home: HomeEn,
     product: ProductEn,
     contact: ContactEn,
+    footer: FooterEn,
   },
 };
 
